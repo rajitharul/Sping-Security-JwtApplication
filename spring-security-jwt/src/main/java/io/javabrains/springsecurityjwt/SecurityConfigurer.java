44a53203231978af362b19eca39d666c106f9a93
rajitharul <62076329+rajitharul@users.noreply.github.com>
2021-03-07 19:49:52 +0530

    build

--- spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/SecurityConfigurer.java
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package io.javabrains.springsecurityjwt;
 
+import io.javabrains.springsecurityjwt.filters.JwtRequestFilter;
 import io.javabrains.springsecurityjwt.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +9,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 //security configure also needs and annotation which is web security
@@ -18,6 +21,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -50,7 +57,11 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         //this disable cross site refference forgery and for /authenticate it permits any ot this. any other requests should be authenticated.
         http.csrf().disable()
                     .authorizeRequests().antMatchers("/authenticate").permitAll().
-                    anyRequest().authenticated();
+                    anyRequest().authenticated()
+                .and().sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter , UsernamePasswordAuthenticationFilter.class);
 
     }
 }
--- spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/filters/JwtRequestFilter.java
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/filters/JwtRequestFilter.java
@@ -1,5 +1,13 @@
 package io.javabrains.springsecurityjwt.filters;
 
+import io.javabrains.springsecurityjwt.services.MyUserDetailsService;
+import io.javabrains.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,10 +16,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+
     @Override
+    //filter chain is taken becuase it has the option of passing on to the next filter in the filter chain.
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        //we first get the usedetails serivce and take the detials form the http request and check whether it is valid.
+
+        final String authorizationHeader = httpServletRequest.getHeader("Authorization");
+
+
+    String username = null;
+    String jwt = null;
+
+    if(authorizationHeader != null &&  authorizationHeader.startsWith("Bearer")){
+        jwt  = authorizationHeader.substring(7);
+        username = jwtUtil.extractUsername(jwt);
+
+    }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
 
     }
 }
