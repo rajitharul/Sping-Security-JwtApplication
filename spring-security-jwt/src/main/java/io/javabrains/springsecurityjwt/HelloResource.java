292d7813df11039f7f739b18c0dd495d9439473c
rajitharul <62076329+rajitharul@users.noreply.github.com>
2021-03-07 19:23:17 +0530

    build

--- spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/HelloResource.java
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/HelloResource.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//Git Connected
+
 @RestController
 public class HelloResource {
 
